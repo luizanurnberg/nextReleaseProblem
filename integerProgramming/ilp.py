fad6dc50f7aacb3f36838ce23f628aceac472981
Mary <52547463+maariaceciliaholler@users.noreply.github.com>
2023-12-02 17:01:46 -0300

    feat(ig.py): adjusted file

--- integerProgramming/ilp.py
+++ integerProgramming/ilp.py
@@ -13,7 +13,6 @@ def solution(model):
         if model.x[i]() == 1: result += ' ' + str(i + 1)
     return result
 
-
 # Formulation 1: general
 def formulation1():
     model = ConcreteModel()
@@ -62,74 +61,9 @@ def formulation1_transformed():
     results = solver.solve(model, timelimit = 1000)
     return model.obj.expr(), results, model
 
-
-# Formulation 2: only customers (requirement costs are used in the constraint only)
-# Work in progress!
-def formulation2():
-    instance.transformation1()
-    
-    model = ConcreteModel()
-    model.y = Var(range(instance.m), domain = Binary, initialize = 0)
-
-    model.obj = Objective(
-        expr = sum(instance.w[i] * model.y[i] for i in range(instance.m)), sense = maximize
-    )
-    
-    def budget_constraint(model):
-        total_cost = 0
-        for i in range(instance.n):
-            for cus in range(instance.m):
-                if value(model.y[cus]) == 1 and i in instance.S[cus]:
-                    total_cost += instance.c[i]
-                    break
-        return total_cost <= instance.b
-        #if total_cost == 0: return Constraint.Skip
-        #return (None, total_cost, instance.b)
-
-    def budget_constraint2(model):
-        total_cost = 0
-        added = [0 for _ in range(instance.n)]
-        for cus in range(instance.m):
-            for i in instance.S[cus]:
-                if added[i] == 0:
-                    total_cost += instance.c[i] * model.y[cus]
-                    added[i] = value(model.y[cus])
-        return total_cost <= instance.b
-
-    #model.cons = Constraint(rule = budget_constraint2)
-    model.c = Constraint(expr = sum(instance.c[i] for cus in range(instance.m) if value(model.y[cus]) == 1 and i in instance.S[cus] for i in range(instance.n)) <= instance.b)
-
-    solver = SolverFactory('glpk')
-    results = solver.solve(model, timelimit = 1000)
-    return model.obj.expr(), results, model
-
-
-# Formulation 3: only requirements (customers define weight of requirements)
-def formulation3():
-    model = ConcreteModel()
-    model.x = Var(range(instance.n), domain = Binary)
-
-    model.obj = Objective(
-        expr = sum(instance.v[i] * model.x[i] for i in range(instance.n)), sense = maximize
-    )
-
-    model.cons = ConstraintList()
-
-    model.cons.add(expr = sum(instance.c[i] * model.x[i] for i in range(instance.n)) <= instance.b)
-
-    for pair in instance.P:
-        model.cons.add(expr = model.x[pair[0]] >= model.x[pair[1]])
-
-    solver = SolverFactory('glpk')
-    results = solver.solve(model, timelimit = 1000)
-    return model.obj.expr(), results, model
-    
-
 def solve(formulation):
     if formulation == '1': return formulation1()
     elif formulation == '1t': return formulation1_transformed()
-    elif formulation == '2': return formulation2()
-    elif formulation == '3': return formulation3()
     print(f'Invalid formulation: {formulation}')
     exit()
 
@@ -145,5 +79,3 @@ instance.read(path, factor)
 value, results, model = solve(formulation)
 print(value)
 if results.solver.termination_condition != TerminationCondition.optimal: print(results.solver.termination_condition)
-#print(results.solver.status)
-#print(results.solver.termination_condition)
